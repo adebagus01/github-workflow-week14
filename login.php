84685d6ce7c344633ce6c5f4b847eaaefdd8fa70
Ade Bagus Pratama <55870942+adebagus01@users.noreply.github.com>
2022-05-27 19:55:46 +0700

    penambahan fitur login

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>FITUR LOGIN</h1>
+    <form action="">
+        <label for="">
+            <input type="text" placeholder="Masukkan nama">
+            <input type="password" placeholder="Masukkan password">
+            <button type="submit">Login</button>
+        </label>
+        <a href="register.php">Belum punya akun? segera daftarkan</a>
+    </form>
+</body>
+</html>
